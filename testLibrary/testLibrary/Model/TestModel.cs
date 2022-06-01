b64712ae4db937c40b91eb77c8caed000aea81c7
Nicholas Henrik Olsen <niho@cloudfactory.dk>
2022-06-01 10:05:21 +0200

    Created a test library repo

---
+++ testLibrary/testLibrary/Model/TestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testLibrary.Model
+{
+    public class TestModel
+    {
+        public string testVariable { get; set; }
+    }
+}
